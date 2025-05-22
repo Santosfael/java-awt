ff9bcf4a7f87f13f8ffe59969c2cf514abc8159f
Rafael Rocha <rafaelsantoscv_@hotmail.com>
2025-05-22 17:20:03 -0300

    feat: create Login screen

---
+++ src/br/com/rafael/Application.java
@@ -0,0 +1,13 @@
+package br.com.rafael;
+
+import br.com.rafael.views.Login;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Login login = new Login();
+		login.sceneLogin();
+
+	}
+
+}
